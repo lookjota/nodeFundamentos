a456dd2dfad53eeabb1a81de44dd7aa88d7ddf43
lookjota <lookjota@gmail.com>
2024-08-31 13:11:17 -0300

    Route e query parameters

--- src/middlewares/routes.js
+++ src/middlewares/routes.js
@@ -33,4 +33,11 @@ export const routes = [
       return res.writeHead(201).end()
     }
   },
+  {
+    method: 'DELETE',
+    path: '/users/1',
+    handler: (req, res) => {
+      return res.end()
+    },
+  }
 ]
