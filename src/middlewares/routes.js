7efc163de4c327bbc30302cf8980242cf5eca1e1
lookjota <lookjota@gmail.com>
2024-09-02 11:40:32 -0300

    Filtrando lista do banco de dados

--- src/middlewares/routes.js
+++ src/middlewares/routes.js
@@ -12,9 +12,12 @@ export const routes = [
     // funcao que vai executar a operacao da rota
     handler: (req, res) => {
 
-      console.log(req.query)
+      const { search } = req.query
 
-      const users = database.select('users')
+      const users = database.select('users', search ? {
+        name: search,
+        email: search,
+      }: null )
     
       return res.end(JSON.stringify(users))
     }
